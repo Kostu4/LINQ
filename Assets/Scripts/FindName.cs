f64a6f19150c3c98dd716252fe448b46043a3ba7
Kostu4 <webcorpgaming@gmail.com>
2024-07-09 21:59:13 +0300

    Fix the Script FindName
    Add method which counts letters of the longest name and display that + all names which end on "s"

--- Assets/Scripts/FindName.cs
+++ Assets/Scripts/FindName.cs
@@ -7,16 +7,21 @@ using UnityEngine;
 public class FindName : MonoBehaviour
 {
     private string[] names = {"John", "Tompson", "Kreks", "Yuto", "kostu4", "Leroy", "Djenkins", "Alis", "Josss", "IamFkingBoss", "Gerde2008s"};
-
     private List<string> Search()
     { 
         List<string> searchedName = names.Where(x => x.EndsWith("s")).OrderByDescending(x => x.Length).Take(1).ToList();
         return searchedName;
+    }
+    private int Search2()
+    {
+        int longName = names.Max(x => x.Length);
+        return longName;
     }
 
     [ContextMenu(nameof(GetName))]
     public void GetName()
     {
         Debug.Log(JsonConvert.SerializeObject(Search()));
+        Debug.Log(JsonConvert.SerializeObject("The longest name has " + Search2() + " letters"));
     }
 }
